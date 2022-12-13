e4efda59f534de70e1c937bb90d6310b43893638
rodrigo <rodrigomsiqq@gmail.com>
2022-12-13 11:31:23 -0300

    adicionado update de cadastro

--- Apps/estacao/urls.py
+++ Apps/estacao/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('',views.home),
     path('registrarEstacao/', views.registrarEstacao),
+    path('edicaoEstacao/<identificador>', views.edicaoEstacao),
+    path('editarEstacao/', views.editarEstacao),
     path('deletarEstacao/<identificador>', views.deletarEstacao)
+    
     
 ]
--- Apps/estacao/views.py
+++ Apps/estacao/views.py
@@ -16,6 +16,28 @@ def registrarEstacao(request):
     estacao = EstacaoTrabalho.objects.create(nome=nome, modelo=modelo, serial=serial, setor=setor)
     return redirect('/')
 
+def edicaoEstacao(request, identificador):
+    estacao = EstacaoTrabalho.objects.get(identificador=identificador)
+    return render(request, "edicaoEstacao.html", {"estacao": estacao})
+    
+
+def editarEstacao(request):
+    nome = request.POST['txtNome']  
+    modelo = request.POST['txtmodelo']
+    serial = request.POST['txtserial']
+    setor = request.POST['txtsetor']
+    
+    estacao = EstacaoTrabalho.objects.get(nome=nome)
+    estacao.nome=nome
+    estacao.modelo=modelo
+    estacao.serial=serial
+    estacao.setor=setor
+    estacao.save()
+    
+    return redirect('/')
+        
+
+
 def deletarEstacao(resquest, identificador):
     estacao = EstacaoTrabalho.objects.get(identificador=identificador)
     estacao.delete()
